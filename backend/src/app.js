cbfb3310879dc6692201b9e1dcf8ade3bad54eeb
Abhay Nagpure <abhaynagpure99@gmail.com>
2026-06-13 14:26:31 +0530

    connecting frontend with backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 //CORS middleware.
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN === "*" ? "*" : [process.env.CORS_ORIGIN, "http://localhost:5173"],
     credentials: true
 }));
 
